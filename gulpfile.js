f6178b459b79890900080bca396990e4b9641dd2
urnmo <aaron0moore@gmail.com>
2016-10-20 13:49:52 -0400

    password

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,43 @@
 'use strict'
 
 let gulp = require('gulp');
+let babel = require("gulp-babel");
 let sass = require('gulp-sass');
 let htmlhint = require('gulp-htmlhint');
 let commentless = require('gulp-strip-css-comments');
 
 gulp.task('default', ['html', 'css', 'js']);
 
-gulp.task('html',function(){
+gulp.task('html', function () {
     return gulp.src('index.html')
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter())
-    .pipe(gulp.dest('public/'))
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(gulp.dest('public/'))
 });
 
-gulp.task('css',function(){
+gulp.task('css', function () {
     return gulp.src('style.scss')
         .pipe(sass())
         .pipe(commentless())
         .pipe(gulp.dest('public/'));
 });
 
-gulp.task('js',function(){
+gulp.task('js', function () {
     return gulp.src('app.js')
-    .pipe(gulp.dest('public/'))
+        .pipe(babel({
+            presets: ["es2015"]
+        }))
+        .pipe(gulp.dest('public/'))
 });
 
-gulp.task('watch', function (){
+gulp.task('watch', function () {
     gulp.watch('index.html', ['html']);
     gulp.watch('style.scss', ['css']);
     gulp.watch('app.js', ['js']);
 })
+
+// gulp.task("default", () => {
+//     return gulp.src("src/app.js")
+
+//         .pipe(gulp.dest("dist"));
+// });
