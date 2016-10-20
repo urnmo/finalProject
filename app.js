5902e7de0577e0e70f44326a01b918b77acac89a
urnmo <aaron0moore@gmail.com>
2016-10-20 14:39:00 -0400

    bug fix

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ app.factory("formsPageService", function ($http) {
 
             $http({
                 method: "POST",
-                url: "https://radiant-brook-98763.herokuapp.com/records/" + fid.Pid.fid + "/" + fidPid.pid,
+                url: "https://radiant-brook-98763.herokuapp.com/records/" + fidPid.fid + "/" + fidPid.pid,
                 data: answers,
             })
         }
--- public/app.js
+++ public/app.js
@@ -268,7 +268,7 @@ app.factory("formsPageService", function ($http) {
 
             $http({
                 method: "POST",
-                url: "https://radiant-brook-98763.herokuapp.com/records/" + fid.Pid.fid + "/" + fidPid.pid,
+                url: "https://radiant-brook-98763.herokuapp.com/records/" + fidPid.fid + "/" + fidPid.pid,
                 data: answers
             });
         }
